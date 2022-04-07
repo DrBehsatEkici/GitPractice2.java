649d3af10cd350017e8570db6f235db301bc9705
DrBehsatEkici <drbehsatekici@gmail.com>
2022-04-07 17:48:58 +0300

    initial commit

---
+++ GitPractice1.java
@@ -0,0 +1,15 @@
+public class GitPractice1 {
+
+    public static void main(String[] args) {
+
+        // doing some work here.
+
+        // hello world
+
+        System.out.println("hello world");
+        System.out.println("basaracaksin");
+////
+
+
+    }
+}
